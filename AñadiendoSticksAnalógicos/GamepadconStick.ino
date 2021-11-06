855f9532e640b0fc11beb216a9de8f1ae85a5698
gustavoalara <gustavoa.laraheredero@gmail.com>
2021-11-06 15:50:48 +0100

    Add files via upload

---
+++ AñadiendoSticksAnalógicos/GamepadconStick.ino
@@ -0,0 +1,69 @@
+//Ejemplo simple de definición y creación del Gamepad en modo DirectInput con la librería de Heironimus
+//Añadiendo ejes X,Y y un botón
+
+
+#include <Joystick.h>
+
+//En este ejemplo tendríamos un Gamepad con un stick X, Y y el botón del propio stick (tipo mando Xbox o Play)
+
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_GAMEPAD,
+  1, 0,                  // Número de Botones (Máx. 128), Número de D-PAD (Máx. 2)
+  true, true, false,     // Ejes X (si volante=false), Y (si acelerador2=false), Z (true= habilitado, false=deshabilitado)
+  false, false, false ,   // Ejes Rx, Ry, Rz(si Freno o Timón=false) (true= habilitado, false=deshabilitado)
+  false, false,          // Timón(si Rz o Freno=false), Acelerador
+  false, false, false);  // Acelerador2(en columna de dirección, si Y=false), freno(si Rz o Timón = false), volante((en columna de dirección,si X=false)
+
+void setup() {
+
+  //Inicializamos los pines de los botones
+  pinMode(2, INPUT_PULLUP); //Inicio el Pin2 en modo entrada y con resistencia 
+
+  // Inicializamos el joystick
+  Joystick.begin();
+  Joystick.setXAxisRange(0,1023); //Definimos el rango del eje X (con los valores mínimos y máximos sacados por el puerto serie)
+  Joystick.setYAxisRange(0,1023); //Lo mismo para el eje Y
+}
+
+void loop() {
+  /*
+  //Gamepad
+  if (!digitalRead(2)) //Si el botón conectado al Pin 2 está pulsado
+  {
+    Joystick.setHatSwitch(0, 0);  //Muevo el D-Pad 0 a  0 grados (Arriba)
+  }
+  if (!digitalRead(3)) //Si el botón conectado al Pin 3 está pulsado
+  {
+    Joystick.setHatSwitch(0, 90);  //Muevo el D-Pad 0 a 90 grados (Derecha)
+  }
+  if (!digitalRead(4)) //Si el botón conectado al Pin 4 está pulsado
+  {
+    Joystick.setHatSwitch(0, 180);  //Muevo el D-Pad 0 a 180 grados (Abajo)
+  }
+  if (!digitalRead(5)) //Si el botón conectado al Pin 5 está pulsado
+  {
+    Joystick.setHatSwitch(0, 270);  //Muevo el D-Pad 0 a 270 grados (Izquierda)
+  }
+
+  if (digitalRead(2) && digitalRead(3) && digitalRead(4) && digitalRead(5)) //Si ninguno de los pines 2,3,4, y 5 están pulsado
+  {
+    Joystick.setHatSwitch(0, -1);  //Dejo el D-Pad 0 al centro
+  }
+  */
+  //Botones
+  if (!digitalRead(2)) //Si el botón conectado al Pin 2 está pulsado
+  {
+    Joystick.setButton(0, 1);  //Activo el botón 0 del gamepad
+  }
+  else Joystick.setButton(0,0); //y si no, desactivo el botón 0 del gamepad
+
+  /*
+  if (!digitalRead(7)) //Si el botón conectado al Pin 7 está pulsado
+  {
+    Joystick.setButton(1, 1);  //Activo el botón 0 del gamepad
+  }
+  else Joystick.setButton(1,0); //y si no, desactivo el botón 0 del gamepad
+  */
+  //Ejes
+  Joystick.setXAxis(analogRead(A0)); //Paso al eje X los valores de la posición del potenciómetro conectado al pin A0
+  Joystick.setYAxis(analogRead(A1)); //Paso al eje Y los valores de la posición del potenciómetro conectado al pin A1
+}
